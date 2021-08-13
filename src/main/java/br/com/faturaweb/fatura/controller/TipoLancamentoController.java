6d8f61d1dbdcc484045a49bbf878696964b03137
Elias Santana <eliasantanasilva@gmail.com>
2021-08-13 07:18:22 -0300

    v1.0.2 - Adicionado ajustes para a funcionalidade Alterar Tipos de Lançamento

--- src/main/java/br/com/faturaweb/fatura/controller/TipoLancamentoController.java
+++ src/main/java/br/com/faturaweb/fatura/controller/TipoLancamentoController.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +19,8 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
+import com.fasterxml.jackson.annotation.JsonCreator.Mode;
+
 import br.com.faturaweb.fatura.model.TipoLancamento;
 import br.com.faturaweb.fatura.repository.TipoLancamentoRepository;
 
@@ -26,47 +29,73 @@ import br.com.faturaweb.fatura.repository.TipoLancamentoRepository;
 @RequestMapping("tipolancamento")
 public class TipoLancamentoController {
 
-	@Autowired	
+	@Autowired
 	private TipoLancamentoRepository tipoLancamentoRepository;
-	
-	
+
 	@GetMapping("listar")
 	public String tipoLancamento(Model model) {
 		List<TipoLancamento> tiposDeLancamentos = tipoLancamentoRepository.findAllTipoLancamentos();
 		model.addAttribute("tipolancamentos", tiposDeLancamentos);
 		return "home/listar-tipos-lancamentos";
 	}
-	
+
 	@GetMapping("cadastro")
-	public String formCadastro() {
+	public String formCadastro(Model model) {
+		TipoLancamento tp = new TipoLancamento();
+		model.addAttribute("tipo", tp);
 		return "home/form-tipolancamento";
 	}
-	
+
 	@PostMapping("adicionar")
 	public String home(@Valid TipoLancamento tipoLancamento) {
-		
-		TipoLancamento tpLancamento = new TipoLancamento(tipoLancamento.getDsTipoLancamento());		
+
+		TipoLancamento tpLancamento = new TipoLancamento(tipoLancamento.getDsTipoLancamento());
 		try {
-			Optional<TipoLancamento> tipolancamentoLocalizado = tipoLancamentoRepository.findBydsTipoLancamento(tpLancamento.getDsTipoLancamento());
+			Optional<TipoLancamento> tipolancamentoLocalizado = tipoLancamentoRepository
+					.findBydsTipoLancamento(tpLancamento.getDsTipoLancamento());
 			TipoLancamento tipoLancamento2 = tipolancamentoLocalizado.get();
-			
-		} catch (NoSuchElementException e) {			
-			tipoLancamentoRepository.save(tpLancamento)	;
+
+		} catch (NoSuchElementException e) {
+			tipoLancamentoRepository.save(tpLancamento);
 		}
 		return "home/form-tipolancamento";
 	}
-	
+
 	@GetMapping("/excluir/{id}")
 	public RedirectView excluir(@PathVariable Long id) {
 		RedirectView redirectView = new RedirectView();
 		redirectView.setUrl("http://localhost:8080/tipolancamento/listar");
-		
-		if (id!=null) {
+
+		if (id != null) {
 			tipoLancamentoRepository.deleteById(id);
-		}else {
+		} else {
 			System.out.println("Lançamento não localizado!");
 		}
-		
+
 		return redirectView;
+	}
+
+	@GetMapping("/alterar/{id}")
+	public String alterar(@PathVariable Long id, Model model, TipoLancamento tpLancamento) {
+
+		Optional<TipoLancamento> tipolancamento = tipoLancamentoRepository.findBycdTipoLancamento(id);
+		TipoLancamento tipoLancamento2 = tipolancamento.get();
+		model.addAttribute("tipo", tipoLancamento2);
+		return "home/form-tipolancamento-atualizar";
+	}
+
+	@PostMapping("/atualizar")
+	public RedirectView atualizar(TipoLancamento tp, Model model) {
+
+		TipoLancamento tipolancamentoForm = tp;
+		Optional<TipoLancamento> tipolancamento = tipoLancamentoRepository
+				.findBycdTipoLancamento(tp.getCdTipoLancamento());
+		
+		tipolancamentoForm.setDtCadastro(tipolancamento.get().getDtCadastro());
+		tipoLancamentoRepository.save(tp);
+		model.addAttribute("tipo", tp);
+		RedirectView rdw = new RedirectView();
+		rdw.setUrl("http://localhost:8080/tipolancamento/listar");
+		return rdw;
 	}
 }
