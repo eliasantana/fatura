dd059477e86b172a7ccef2363ff58a829ff7972b
Elias Santana <eliasantanasilva@gmail.com>
2021-08-15 07:51:34 -0300

    v1.0.2 - Adicionado funcionalidade Salvar, Alterar, Excluir para forma de pagamento

---
+++ src/main/java/br/com/faturaweb/fatura/repository/FormaDePagamentoRepository.java
@@ -0,0 +1,19 @@
+package br.com.faturaweb.fatura.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.faturaweb.fatura.model.FormaDePagamento;
+@Repository
+public interface FormaDePagamentoRepository extends CrudRepository<FormaDePagamento, Long> {
+
+	@Query("Select fp from FormaDePagamento fp")
+    List<FormaDePagamento> findAllFormasDePagamento();
+	
+	@Query("Select fp from FormaDePagamento fp where fp.descricao =:descricao ")
+   Optional<FormaDePagamento> findByDescricaoFormaDePagamento(String descricao);
+}
