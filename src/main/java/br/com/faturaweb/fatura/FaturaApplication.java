7485f62601f0c70ef3153f9b9f2b30d13204e5be
Elias Santana <eliasantanasilva@gmail.com>
2021-10-11 06:49:01 -0300

    Remoção da notação @RestController

--- src/main/java/br/com/faturaweb/fatura/FaturaApplication.java
+++ src/main/java/br/com/faturaweb/fatura/FaturaApplication.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 @ComponentScan
 
 public class FaturaApplication {
