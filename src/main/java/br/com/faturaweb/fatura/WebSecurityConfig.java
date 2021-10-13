a49869f31d124d06b27f0077e1f8f3adc1f95dd6
Elias Santana <eliasantanasilva@gmail.com>
2021-10-13 13:24:46 -0300

    Tete de permissão

--- src/main/java/br/com/faturaweb/fatura/WebSecurityConfig.java
+++ src/main/java/br/com/faturaweb/fatura/WebSecurityConfig.java
@@ -20,6 +20,7 @@ public class WebSecurityConfig  extends WebSecurityConfigurerAdapter 	{
 			protected void configure(HttpSecurity http) throws Exception {
 				http
 					.authorizeRequests()
+					.antMatchers("/lancamento/**").permitAll()
 					.anyRequest().authenticated()
 					.and()
 					.formLogin(form -> form
