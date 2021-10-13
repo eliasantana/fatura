682183aeefb1f1eaf44cee03c425439042fe1b0c
Elias Santana <eliasantanasilva@gmail.com>
2021-10-13 13:34:00 -0300

    V1.0.14 - ajustes de permissão

--- src/main/java/br/com/faturaweb/fatura/WebSecurityConfig.java
+++ src/main/java/br/com/faturaweb/fatura/WebSecurityConfig.java
@@ -20,7 +20,7 @@ public class WebSecurityConfig  extends WebSecurityConfigurerAdapter 	{
 			protected void configure(HttpSecurity http) throws Exception {
 				http
 					.authorizeRequests()
-					.antMatchers("/lancamento/**").permitAll()
+					.antMatchers("/**").permitAll()
 					.anyRequest().authenticated()
 					.and()
 					.formLogin(form -> form
