1a97bc1a0550ff379acc0fa78ef5bbbe5206c101
Elias Santana <eliasantanasilva@gmail.com>
2021-10-14 20:04:48 -0300

    bugfix/#v1.16 - Ajustes security

--- src/main/java/br/com/faturaweb/fatura/WebSecurityConfig.java
+++ src/main/java/br/com/faturaweb/fatura/WebSecurityConfig.java
@@ -20,7 +20,7 @@ public class WebSecurityConfig  extends WebSecurityConfigurerAdapter 	{
 			protected void configure(HttpSecurity http) throws Exception {
 				http
 					.authorizeRequests()
-					.antMatchers("**").permitAll()
+					.antMatchers("/lancamento/**").permitAll()
 					.anyRequest().authenticated()
 					.and()
 					.formLogin(form -> form
