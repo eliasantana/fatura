dd57d1cbc2a1b5fb2157533ebace346f22ab31a1
Elias Santana <eliasantanasilva@gmail.com>
2021-10-16 07:57:00 -0300

    teste

--- src/main/java/br/com/faturaweb/fatura/controller/HomeController.java
+++ src/main/java/br/com/faturaweb/fatura/controller/HomeController.java
@@ -1,28 +1,35 @@
 package br.com.faturaweb.fatura.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import br.com.faturaweb.fatura.model.Lancamento;
 import br.com.faturaweb.fatura.repository.LancamentoRepository;
 @ComponentScan
 @Controller
 
 public class HomeController {
 	@Autowired
-	LancamentoRepository LancamentoRepository;
+	LancamentoRepository lancamentoRepository;
 	
 	@GetMapping("/")
 	public String index(Model model) {
 		return "home/dashboard";
 	}
 	
-	@GetMapping("teste")
+	@GetMapping("/listar")
 	public String listar(Model model) {
-	
-		return "home/listar";
+		List<Lancamento> lancamentos = lancamentoRepository.findAllLancamentosDoMes();
+		System.out.println("listando");
+		model.addAttribute("lancamentos", lancamentos);
+
+		return "home/listar-lancamento";
 	}
+
 	
 }
