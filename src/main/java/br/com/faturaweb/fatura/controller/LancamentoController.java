96be8ba19e241631d9197f837fa141c2c2a3f293
Elias Santana <eliasantanasilva@gmail.com>
2021-10-15 23:29:12 -0300

    Ajuste

--- src/main/java/br/com/faturaweb/fatura/controller/LancamentoController.java
+++ src/main/java/br/com/faturaweb/fatura/controller/LancamentoController.java
@@ -94,14 +94,14 @@ public class LancamentoController {
 		return "lancamento/listar-lancamento";
 	}
 
-	@GetMapping("/listar")
+	@GetMapping("listar")
 	public String listar(Model model) {
 		//List<Lancamento> lancamentos = lancamentoRepository.findAllLancamentos();
 		List<Lancamento> lancamentos = lancamentoRepository.findAllLancamentosDoMes();
 		System.out.println("listando");
 		model.addAttribute("lancamentos", lancamentos);
 
-		return "listar-lancamento";
+		return "lancamento/listar-lancamento";
 	}
 
 	@GetMapping("excluir/{id}")
