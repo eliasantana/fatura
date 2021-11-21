7f3cd8cf1de7771bf37ab5b5819bc674b87f0c8c
Elias Santana <eliasantanasilva@gmail.com>
2021-11-21 07:50:07 -0300

    Adicionado TesteController para realização de testes gerais

--- src/main/java/br/com/faturaweb/fatura/controller/LoginController.java
+++ src/main/java/br/com/faturaweb/fatura/controller/LoginController.java
@@ -1,13 +1,28 @@
 package br.com.faturaweb.fatura.controller;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
 	
 	@GetMapping("/login")
-	public String login() {
+	public String login(Model model) {
+		Map<String, Integer>data = new LinkedHashMap<String, Integer>();
+		data.put("Janeiro", 200);
+		data.put("Fevereiro", 300);
+		data.put("Março", 150);
+		data.put("Abril", 400);
+		data.put("Maio", 800);
+		data.put("Junho", 950);
+		
+		model.addAttribute("keyset",data.keySet());
+		model.addAttribute("values",data.values());
+		
 		
 		return "login";
 	}
---
+++ src/main/java/br/com/faturaweb/fatura/model/Faturamento.java
@@ -0,0 +1,14 @@
+package br.com.faturaweb.fatura.model;
+
+import java.math.BigDecimal;
+
+public class Faturamento {
+	private long id;
+	private Integer mes;
+	private BigDecimal total;
+	
+	public Faturamento() {
+		
+	}
+	
+}
