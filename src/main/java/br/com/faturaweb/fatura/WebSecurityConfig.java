7cfc64e5c7455db0660c1da201316b150e2d2f9e
Elias Santana <eliasantanasilva@gmail.com>
2022-01-16 06:54:39 -0300

    Ajustes gerais

--- src/main/java/br/com/faturaweb/fatura/WebSecurityConfig.java
+++ src/main/java/br/com/faturaweb/fatura/WebSecurityConfig.java
@@ -29,8 +29,7 @@ public class WebSecurityConfig  extends WebSecurityConfigurerAdapter 	{
 			protected void configure(HttpSecurity http) throws Exception {
 				http
 					.authorizeRequests()
-					.antMatchers("/listar").hasAnyRole("ADM")
-					.antMatchers("/getlogo").hasAnyRole("ADM")
+					.antMatchers("/listar").hasAnyRole("ADM")				
 					.anyRequest().authenticated()
 					.and()
 					.formLogin(form -> form
