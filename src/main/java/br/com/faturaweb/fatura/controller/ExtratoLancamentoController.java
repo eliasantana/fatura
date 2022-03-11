b978cf643c41dc3defbba0dccb643f371e7f70e5
Elias Santana <eliasantanasilva@gmail.com>
2022-03-11 16:21:10 -0300

    Adicionando relatório de extrato de despesas

---
+++ src/main/java/br/com/faturaweb/fatura/controller/ExtratoLancamentoController.java
@@ -0,0 +1,49 @@
+package br.com.faturaweb.fatura.controller;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.faturaweb.fatura.model.Lancamento;
+import br.com.faturaweb.fatura.repository.LancamentoRepository;
+import br.com.faturaweb.fatura.services.LancamentoServices;
+
+@Controller
+@RequestMapping("/extrato")
+
+
+public class ExtratoLancamentoController {
+	@Autowired
+	LancamentoRepository lctoRepository;
+	@Autowired
+	LancamentoServices lctoServices;
+	@GetMapping("/financeiro/{mesAno}")
+	public String extratoLancamento(@PathVariable String mesAno, Model model) {
+		String mesEano = null;
+		int year = LocalDate.now().getYear();
+		mesAno =mesAno+  Integer.toString(year);
+		List<Lancamento> lctoFuturo = lctoRepository.findLancamentosFuturos(mesAno);
+		System.out.println(mesEano);
+		HashMap<String, BigDecimal> totalizacaoDespesaCategoria = lctoServices.totalizacaoDespesaCategoria(mesAno);
+		
+		model.addAttribute("lcto",lctoFuturo);
+		model.addAttribute("totalizacao",totalizacaoDespesaCategoria.keySet().toArray());
+		model.addAttribute("valores",totalizacaoDespesaCategoria.values());
+		model.addAttribute("keygrupodespesa",totalizacaoDespesaCategoria.keySet());
+		model.addAttribute("grupovalues",totalizacaoDespesaCategoria.values());
+		String competencia =" Relatório de Despesas Competência - " + mesAno.substring(0,2).concat("/").concat(String.valueOf(year));
+		
+		model.addAttribute("competencia",competencia);
+		return "extrato_lancamento";
+	}
+	
+
+}
--- src/main/java/br/com/faturaweb/fatura/controller/ItMEtaController.java
+++ src/main/java/br/com/faturaweb/fatura/controller/ItMEtaController.java
@@ -5,6 +5,8 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
+import javax.swing.tree.AbstractLayoutCache;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,13 +16,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
 import br.com.faturaweb.fatura.model.Conta;
+import br.com.faturaweb.fatura.model.FormaDePagamento;
 import br.com.faturaweb.fatura.model.ItMeta;
+import br.com.faturaweb.fatura.model.Lancamento;
 import br.com.faturaweb.fatura.model.LogMovimentacaoFinanceira;
 import br.com.faturaweb.fatura.model.Meta;
+import br.com.faturaweb.fatura.model.TipoLancamento;
+import br.com.faturaweb.fatura.model.Usuario;
 import br.com.faturaweb.fatura.repository.ContaRepository;
+import br.com.faturaweb.fatura.repository.FormaDePagamentoRepository;
 import br.com.faturaweb.fatura.repository.ItMetaRepository;
+import br.com.faturaweb.fatura.repository.LancamentoRepository;
 import br.com.faturaweb.fatura.repository.LogMovimentacaoFinanceiraRepository;
 import br.com.faturaweb.fatura.repository.MetaRepository;
+import br.com.faturaweb.fatura.repository.TipoLancamentoRepository;
 import br.com.faturaweb.fatura.services.AppServices;
 import br.com.faturaweb.fatura.services.MetaService;
 
@@ -36,7 +45,12 @@ public class ItMEtaController {
 	ContaRepository contaRepository;
 	@Autowired
 	AppServices appServices;
-	
+	@Autowired
+	FormaDePagamentoRepository formaPagtoRepository;
+	@Autowired
+	LancamentoRepository lancamentoRepository;
+	@Autowired
+	TipoLancamentoRepository tipoLancamentoRepository;
 	@Autowired
 	LogMovimentacaoFinanceiraRepository log;
 	@Autowired
@@ -84,7 +98,7 @@ public class ItMEtaController {
 		RedirectView rw = new RedirectView("http://localhost:8080/itmeta/listar/"+idMeta);
 		ItMeta itMeta = itMetaRepository.findItMetaId(idItMeta);
 		LogMovimentacaoFinanceira logMovimentacao = new LogMovimentacaoFinanceira();
-	
+		LogMovimentacaoFinanceira logLancamentoCredito = new LogMovimentacaoFinanceira();
 		Optional<Meta> metaLocalizada = metaRepository.findById(idMeta);
 		
 		List<ItMeta> itemCreditados = itMetaRepository.findItMeta(idMeta);
@@ -108,6 +122,31 @@ public class ItMEtaController {
 			//Muda o status para creditado
 			itMeta.setSnCreditado("S");
 			itMetaRepository.save(itMeta);
+			
+			// Gerando um lançamento despesas para a meta paga
+			Lancamento lancamento = new Lancamento();
+			lancamento.setDsLancamento("Pagamento Meta "+metaLocalizada.get().getDescricao());
+			lancamento.setDtCadastro(LocalDate.now());
+			lancamento.setDtCompetencia(LocalDate.now());
+			Optional<FormaDePagamento> formaPagto = formaPagtoRepository.findById(5L);
+			lancamento.setFormaDePagamento(formaPagto.get());
+			lancamento.setNrParcela(1);
+			lancamento.setSnPago("SIM"); //Pago
+			Optional<TipoLancamento> tl = tipoLancamentoRepository.findBycdTipoLancamento(15L);
+			lancamento.setTipoLancamento(tl.get());
+			Usuario u = new Usuario();
+			u.setCdUsuario(5L);
+			lancamento.setUsuario(u);//TESTES
+			lancamento.setVlPago(itMeta.getVlrSemana());
+			
+			lancamentoRepository.save(lancamento);
+			//Gerano um log para o credito lançado
+			logLancamentoCredito.setDescricao(lancamento.getDsLancamento());
+			logLancamentoCredito.setDtMovimentacao(LocalDate.now());
+			logLancamentoCredito.setNrConta(conta.getNrConta());
+			logLancamentoCredito.setVlMovimentado(lancamento.getVlPago());
+			logLancamentoCredito.setTpMovimentacao("C");
+			log.save(logLancamentoCredito);
 		}
 		// Dados do Gráfico 
 			model.addAttribute("totalmeta",totalItMeta);
