901773371bf1109922ac938cc4b0cbb9a12bccc1
Elias Santana <eliasantanasilva@gmail.com>
2022-09-02 23:41:29 -0300

    v.1.68 | LOTE: Ajuste na view | RECEITA - Cédito e Débito

--- src/main/java/br/com/faturaweb/fatura/controller/ReceitaController.java
+++ src/main/java/br/com/faturaweb/fatura/controller/ReceitaController.java
@@ -1,5 +1,6 @@
 package br.com.faturaweb.fatura.controller;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
@@ -13,7 +14,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
+import br.com.faturaweb.fatura.model.Configuracoes;
+import br.com.faturaweb.fatura.model.Conta;
+import br.com.faturaweb.fatura.model.LogMovimentacaoFinanceira;
 import br.com.faturaweb.fatura.model.Receita;
+import br.com.faturaweb.fatura.repository.ConfiguracoesRepository;
+import br.com.faturaweb.fatura.repository.ContaRepository;
+import br.com.faturaweb.fatura.repository.LogMovimentacaoFinanceiraRepository;
 import br.com.faturaweb.fatura.repository.ReceitaRepository;
 import br.com.faturaweb.fatura.services.ReceitaServices;
 
@@ -23,9 +30,14 @@ public class ReceitaController {
 	
 	@Autowired
 	ReceitaRepository receitaRepository;
-	
+	@Autowired
+	ContaRepository contaRepository;
+	@Autowired
+	LogMovimentacaoFinanceiraRepository logRepository;
 	@Autowired
 	ReceitaServices services;
+	@Autowired
+	ConfiguracoesRepository configuracoesRepository;
 	
 	@GetMapping("/listar")	
 	public String  listar(Model model) {
@@ -45,13 +57,35 @@ public class ReceitaController {
 	}
 	
 	@PostMapping("/salvar")
-	public String salvar(Model model, Receita recetaForm) {
+	public String salvar(Model model, Receita receitaForm) {
+		LogMovimentacaoFinanceira log = new LogMovimentacaoFinanceira();
 		try {
-			receitaRepository.save(recetaForm);
-			//model.addAttribute("mensagem","Receita salva com sucesso!");
+			receitaRepository.save(receitaForm);			
 			model.addAttribute("mensagem","Receita Salva com sucesso!");
+			//Obtendo a conta configurada
+			Configuracoes config = configuracoesRepository.findConfiguracao();
+			Optional<Conta> contaLocalizada = contaRepository.findConta(config.getNrContaOrigem());
+			if(contaLocalizada.isPresent()) {
+				//obtendo o  saldo e somando a receita líquida 
+				BigDecimal saldo = contaLocalizada.get().getSaldo();
+				BigDecimal novoSaldoBigDecimal = saldo.add(receitaForm.getSalLiquido());
+				contaLocalizada.get().setSaldo(novoSaldoBigDecimal);
+				//Salvando a conta com o novo saldo
+				contaRepository.save(contaLocalizada.get());
+				
+				//registrando a movimentação no log
+				//Registrando no log a movimentação financeira
+				log.setDescricao("Creditando " +receitaForm.getSalLiquido()+" na "+ contaLocalizada.get().getNrConta() + " -"  + contaLocalizada.get().getDsConta());
+				log.setDtMovimentacao(LocalDate.now());
+				log.setNrConta(contaLocalizada.get().getNrConta());
+				log.setTpMovimentacao("C");
+				log.setUsuario("Elias");
+				log.setVlMovimentado(receitaForm.getSalLiquido());
+				logRepository.save(log);
+			}
+				
 		} catch (Exception e) {
-			  System.out.println("Não foi possível salvar a receita informada ->"+ recetaForm.getDsReceita());
+			  System.out.println("Não foi possível salvar a receita informada ->"+ receitaForm.getDsReceita());
 			  model.addAttribute("mensagem","Falha ao tentar savar a receita!");
 		}
 		model.addAttribute("receita",new Receita());
@@ -60,12 +94,35 @@ public class ReceitaController {
 	
 	@GetMapping("excluir/{id}")
 	public RedirectView excluir(@PathVariable Long id, Model model) {
+		//Obtendo a conta de origem
+		LogMovimentacaoFinanceira log = new LogMovimentacaoFinanceira();
+		Configuracoes config = configuracoesRepository.findConfiguracao();
+		Optional<Conta> contaLocalizada = contaRepository.findConta(config.getNrContaOrigem());
 		RedirectView rw = new RedirectView();
-		rw.setUrl("http://localhost:8080/receita/listar");
+		
+		rw.setUrl("/receita/listar");
+		//Localizando a receita a ser excluída
 		Optional<Receita> receita = receitaRepository.findById(id);
+		//Se a receita for localizada, debita o valor da conta  e salva com o novo saldo.
+		if (receita.isPresent()) {
+			BigDecimal saldo = contaLocalizada.get().getSaldo();
+			BigDecimal novoSaldo = saldo.subtract(receita.get().getSalLiquido());
+			contaLocalizada.get().setSaldo(novoSaldo);
+			contaRepository.save(contaLocalizada.get());
+			//Registrando no log a movimentação financeira
+			log.setDescricao("Debitando " + receita.get().getSalLiquido() +" na "+ contaLocalizada.get().getNrConta() + " -"  + contaLocalizada.get().getDsConta());
+			log.setDtMovimentacao(LocalDate.now());
+			log.setNrConta(contaLocalizada.get().getNrConta());
+			log.setTpMovimentacao("D");
+			log.setUsuario("Elias");
+			log.setVlMovimentado(receita.get().getSalLiquido());
+			logRepository.save(log);
+		}
 		if (receita.get().getCdReceita()!=null){
 			receitaRepository.delete(receita.get());
+			
 		}
+		
 		
 		return rw;
 	}
@@ -86,6 +143,9 @@ public class ReceitaController {
 	 * */
 	@GetMapping("/clonar")
 	public RedirectView clonaReceita() {
+		Configuracoes config = configuracoesRepository.findConfiguracao();
+		Optional<Conta> contaLocalizada = contaRepository.findConta(config.getNrContaOrigem());
+		LogMovimentacaoFinanceira log = new LogMovimentacaoFinanceira();
 		
 		Receita maxReceita = receitaRepository.findMaxReceita();
 		Receita r = new Receita();
@@ -96,6 +156,23 @@ public class ReceitaController {
 		r.setSalLiquido(maxReceita.getSalLiquido());
 		
 		receitaRepository.save(r);
+		
+		if (contaLocalizada.isPresent()) {
+			BigDecimal saldo = contaLocalizada.get().getSaldo();
+			BigDecimal novoSaldo = saldo.add(r.getSalLiquido());
+			contaLocalizada.get().setSaldo(novoSaldo);
+			contaRepository.save(contaLocalizada.get());
+			
+			//Registrando no log a movimentação financeira
+			log.setDescricao("Creditando " +r.getSalLiquido()+" na "+ contaLocalizada.get().getNrConta() + " -"  + contaLocalizada.get().getDsConta());
+			log.setDtMovimentacao(LocalDate.now());
+			log.setNrConta(contaLocalizada.get().getNrConta());
+			log.setTpMovimentacao("C");
+			log.setUsuario("Elias");
+			log.setVlMovimentado(r.getSalLiquido());
+			logRepository.save(log);
+		}
+		
 		RedirectView rw = new RedirectView("/receita/listar");
 		//rw.setUrl("http://localhost:8080/receita/listar");
 		
--- src/main/java/br/com/faturaweb/fatura/repository/LogProvisaoRepository.java
+++ src/main/java/br/com/faturaweb/fatura/repository/LogProvisaoRepository.java
@@ -12,4 +12,9 @@ public interface LogProvisaoRepository extends CrudRepository<LogProvisao, Long>
 	@Query(value = "select *   from log_provisao  where competencia =  date_format(curdate(),'%m%Y')", nativeQuery = true)
 	List<LogProvisao> findAllLogProvisaoDaCompetencia();
 	
+	/**
+	 * Retorna os itens provisionados da competencia informada
+	 * */
+	@Query(value =  "select *   from log_provisao  where competencia  = :p_competencia", nativeQuery = true)
+	List<LogProvisao>findLogProvisaoDaCompetencia(String p_competencia );
 }
